0ebec5fa1664fc7aab3d0bb18e5cea6846ce85ba
Tooraj Taraz <tooraj.info@gmail.com>
2021-07-17 15:48:07 +0430

    first phase done

--- migration/share_ssh_keys.sh
+++ migration/share_ssh_keys.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-MY_PATH="`dirname \"$0\"`"                        # relative
-MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
-if [ -z "$MY_PATH" ] ; then
-  # error; for some reason, the path is not accessible
-  # to the script (e.g. permissions re-evaled after suid)
-  exit 1  # fail
-fi
-
 #define style and color for printing logs
 BOLD=$(tput bold);
 NORMAL=$(tput sgr0);
 GREEN='\033[0;92m';
 RED='\033[0;91m';
 NC='\033[0m'
+current_pid=$$
 
 compute_nodes=()
+compute_names=()
 
 input_computes=$1
 
@@ -29,17 +23,29 @@ do
   fi
   echo "compute <$compute> <$compute_ip> added to the list."
   compute_nodes+=($compute_ip)
+  compute_names+=($compute)
 done
 
-
+create_directory="mkdir -p /root/.ssh"
+create_file="/root/.ssh/authorized_keys"
+temp="ssh-keygen -t ed25519 -N "" -C "" -f /root/.ssh/$compute"
 for i in "${!compute_nodes[@]}"
 do
   for j in $(seq $(( $i + 1 )) $(( ${#compute_nodes[@]} - 1 )) )
   do
     echo "swapping ${compute_nodes[i]} and ${compute_nodes[j]}"
+    $( ssh  ${compute_nodes[i]} $create_directory ) && $( ssh  ${compute_nodes[j]} $create_directory ) && \
+    $( ssh  ${compute_nodes[i]} $create_file ) && $( ssh  ${compute_nodes[j]} $create_file ) && \
+    echo "success creating dir and file" && \
+    $( ssh  ${compute_nodes[i]} "ssh-keygen -t ed25519 -N '' -C '' -f '/root/.ssh/${compute_names[i]}'" ) && $( ssh  ${compute_nodes[j]} "ssh-keygen -t ed25519 -N '' -C '' -f '/root/.ssh/${compute_names[j]}'" ) && \
+    echo "success generating ssh keys" && \
+    $( scp  "${compute_nodes[i]}:/root/.ssh/${compute_names[i]}.pub" "/tmp/$($current_pid)-${compute_names[i]}" ) && $( scp  "${compute_nodes[j]}:/root/.ssh/${compute_names[j]}.pub" "/tmp/$($current_pid)-${compute_names[j]}" ) && \
+    echo "success getting PUB KEYs" && \
+    $( scp "/tmp/$($current_pid)-${compute_names[j]}" "${compute_nodes[i]}:/tmp" ) && $( scp "/tmp/$($current_pid)-${compute_names[i]}" "${compute_nodes[j]}:/tmp" ) && \
+    echo "success transmitting swapped PUB KEYs" && \
+    $( ssh  ${compute_nodes[i]} "echo '/tmp/$($current_pid)-${compute_names[j]}' >> '/root/.ssh/authorized_keys'" ) && $( ssh  ${compute_nodes[j]} "echo '/tmp/$($current_pid)-${compute_names[i]}' >> '/root/.ssh/authorized_keys'" ) &&
+    echo "success adding swapped PUB KEYs to respective authorized_keys" && continue
+
+    echo "FAILURE"
   done
 done
-
-
-
-
